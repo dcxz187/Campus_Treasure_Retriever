b2c771dd10c9f1955d425391c9dfe671227fa3cf
yyc187 <153188022+yyc187@users.noreply.github.com>
2025-02-23 23:06:36 +0800

    修改mysql信息

--- LostAndFoundOnCampus-backend/src/main/java/com/gongsir/wxapp/WxappApplication.java
+++ LostAndFoundOnCampus-backend/src/main/java/com/gongsir/wxapp/WxappApplication.java
@@ -6,13 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author 龚涛
- * @date 2019-10-23 21:23:36
+ * &#064;date  2019-10-23 21:23:36
  * app启动类
  */
 @SpringBootApplication
 //        (exclude = {SecurityAutoConfiguration.class})
-/**
- * 扫描mapper接口
+/*
+  扫描mapper接口
  */
 @MapperScan("com.gongsir.wxapp.mapper")
 public class WxappApplication {
